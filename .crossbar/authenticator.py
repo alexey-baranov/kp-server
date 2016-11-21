378897256cd58d9a8d6b079d9503a231312a85b4
alexey_baranov <alexey_baranov@inbox.ru>
2016-11-21 18:19:30 +0500

    +authenticator.py

--- .crossbar/authenticator.py
+++ .crossbar/authenticator.py
@@ -1,31 +1,3 @@
-###############################################################################
-##
-##  Copyright (C) Tavendo GmbH and/or collaborators. All rights reserved.
-##
-##  Redistribution and use in source and binary forms, with or without
-##  modification, are permitted provided that the following conditions are met:
-##
-##  1. Redistributions of source code must retain the above copyright notice,
-##     this list of conditions and the following disclaimer.
-##
-##  2. Redistributions in binary form must reproduce the above copyright notice,
-##     this list of conditions and the following disclaimer in the documentation
-##     and/or other materials provided with the distribution.
-##
-##  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-##  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-##  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-##  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-##  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-##  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-##  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-##  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-##  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-##  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-##  POSSIBILITY OF SUCH DAMAGE.
-##
-###############################################################################
-
 import os
 from pprint import pprint
 
@@ -36,31 +8,11 @@ from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession
 from autobahn.wamp.exception import ApplicationError
 
+import psycopg2
+import os
+import json
 
-MYTICKET = os.environ.get('MYTICKET', None)
-if MYTICKET and six.PY2:
-   MYTICKET = MYTICKET.decode('utf8')
-
-# our principal "database"
-PRINCIPALS_DB = {
-   u"alexey_baranov@inbox.ru": {
-      u"ticket": u"***",
-      u"role": u"kopnik"
-   },
-   u"unittest2@domain.ru": {
-      u"ticket": u"***",
-      u"role": u"kopnik"
-   },
-   u"server": {
-      u"ticket": u"***",
-      u"role": u"server"
-   },
-   u"client2": {
-      u"ticket": MYTICKET,
-      u"role": u"frontend"
-   }
-}
-
+CFG_FILENAME='../cfg/config.json'
 
 class AuthenticatorSession(ApplicationSession):
 
@@ -72,15 +24,31 @@ class AuthenticatorSession(ApplicationSession):
          print("WAMP-Ticket dynamic authenticator invoked: realm='{}', authid='{}', ticket='{}'".format(realm, authid, ticket))
          pprint(details)
 
-         if authid in PRINCIPALS_DB:
-            #if ticket == PRINCIPALS_DB[authid]['ticket']:
-            if ticket == authid:
-                print("com.example.auth_success");
-                return PRINCIPALS_DB[authid]['role']
-            else:
-               raise ApplicationError("com.example.invalid_ticket", "could not authenticate session - invalid ticket '{}' for principal {}".format(ticket, authid))
-         else:
-            raise ApplicationError("com.example.no_such_user", "could not authenticate session - no such principal {}".format(authid))
+         db_conn = None
+         conf_name = os.environ.get('NODE_ENV', 'development')
+         if not os.path.exists(CFG_FILENAME):
+             raise Exception('File config not found')
+         data = open(CFG_FILENAME, 'r').read()
+         params = json.loads(data).get(conf_name)
+         if params is None:
+             raise Exception('Config section %s not found' % conf_name)
+         try:
+             db_conn = psycopg2.connect(
+                 host=params.get('host'),
+                 database=params.get('database'),
+                 user=params.get('username'),
+                 password=params.get('password')
+             )
+         except:
+             raise Exception('Database connection error')
+
+         cursor = db_conn.cursor()
+         cursor.execute('SELECT * FROM "Kopnik" WHERE email=\'%s\' and password=\'%s\';' % (authid, ticket))
+         rowcount = cursor.rowcount
+         db_conn.commit()
+         if rowcount == 0:
+             raise ApplicationError("com.example.no_such_user", "could not authenticate session - no such principal {}".format(authid))
+         return 'kopnik'
 
       try:
          yield self.register(authenticate, 'com.example.authenticate')
