a48e30a1f79cdf8d74e0c49e2ef2a238c5d2aa13
alexey_baranov <alexey_baranov@inbox.ru>
2017-02-16 19:46:22 +0500

    +upload

--- src/model/File.js
+++ src/model/File.js
@@ -7,19 +7,31 @@
  * @param {Object} DataTypes
  * @returns {Model}
  */
-module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('File', {
-        id: {
-            type: DataTypes.BIGINT,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        path: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        note: {
-            type: DataTypes.TEXT
-        }
-    });
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('File', {
+    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    path: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    note: {
+      type: DataTypes.TEXT
+    },
+    mimeType: {
+      type: DataTypes.STRING
+    },
+    size: {
+      type: DataTypes.BIGINT
+    }
+  }, {
+    instanceMethods: {}
+  });
 };
